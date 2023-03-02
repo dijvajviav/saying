2800484645845661620841af74d48c1bfa319287
dijvajviav <ckrhksgh555@naver.com>
2023-03-02 18:03:06 +0900

    8단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -38,6 +38,7 @@ public class Main {
                     System.out.println(str[0]+" / " + str[1] + " / "+str[2]); // 실질적인 목록 출력 부분
                 }
                 Collections.reverse(saylist); //  saylist안의 순서를 다시 뒤집어준다.
+
             } else if (cmd_1.length ==2 && cmd_1[0].equals("삭제?id")) {
                 if(remove_saylist.contains(cmd_1[1])){
                     System.out.println(cmd_1[1]+"번 명언은 존재하지 않습니다.");
@@ -49,14 +50,41 @@ public class Main {
                             String fake = fake_list[j];
                             if(fake.equals(cmd_1[1])){
                                 saylist.remove(k);
+                                System.out.println("이때 k의 값은 "+ k + "입니다.");
                             }
-                        }
+                            }
                         k--;
+                        }
                     }
                     System.out.println(cmd_1[1]+"번 명언이 삭제되었습니다.");
                     remove_saylist.add(cmd_1[1]);
+
+                } else if(cmd_1.length ==2 && cmd_1[0].equals("수정?id")) {
+            if (remove_saylist.contains(cmd_1[1])) {
+                System.out.println(cmd_1[1] + "번 명언은 존재하지 않습니다.");
+            } else {
+                int k = saylist.size() - 1;
+                for (int i = (saylist.size() - 1); i > -1; i--) {
+                    String[] fake_list = saylist.get(i);
+                    for (int j = (fake_list.length - 1); j > -1; j--) {
+                        String fake = fake_list[j];
+                        if (fake.equals(cmd_1[1])) {
+                            array[0] = cmd_1[1];
+                            System.out.println("명언(기존) : "+ fake_list[1]);
+                            System.out.printf("명언 : ");
+                            array[1] = scanner.nextLine();
+                            System.out.println("작가(기존) : " + fake_list[2]);
+                            System.out.printf("작가 : ");
+                            array[2] = scanner.nextLine();
+                            saylist.set(k,array);
+
+                        }
+                    }
+                    k--;
                 }
-            } else{
+            }
+        }
+            else{
                 System.out.println("올바른 명령어를 입력하세요.");
             }
 
